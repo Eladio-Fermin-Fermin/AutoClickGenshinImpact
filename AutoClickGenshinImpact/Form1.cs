cb8d7c1973e34edd6e404784abb0d70e48fc19fb
Eladio Fermin Fermin <eladio_fermin@ucne.edu.do>
2023-05-29 17:05:26 -0400

    Se añadio el codigo para llamar al click izquierdo y derecho.

--- AutoClickGenshinImpact/Form1.cs
+++ AutoClickGenshinImpact/Form1.cs
@@ -7,12 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+//Es necesario poner este using...
 using System.Runtime.InteropServices;
 
 namespace AutoClickGenshinImpact
 {
     public partial class Form1 : Form
     {
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwflags, int dx, int dy, int cbuttons, int dwExtraInfo);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetCursorPos(out Point lpPoint);
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Keys vkeys);
+        public static int xPos, yPos;
+
         public Form1()
         {
             InitializeComponent();
